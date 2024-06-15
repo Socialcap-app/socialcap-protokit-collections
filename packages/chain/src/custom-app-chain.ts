f80ac463dfa269a90b0c8ec564a3932e784cfaee
Mario Zito <mario.zito@treme.io>
2024-06-15 17:32:30 -0300

    Implement custom AppChain running in Node with InMemorySigner and config remote Graphql endpoint. Add test for it

---
+++ packages/chain/src/custom-app-chain.ts
@@ -0,0 +1,254 @@
+import "reflect-metadata";
+import { log } from "@proto-kit/common";
+import { InMemoryStateService, Runtime, RuntimeModulesRecord } from "@proto-kit/module";
+import { MandatoryProtocolModulesRecord, Protocol, ProtocolModulesRecord, StateServiceProvider } from "@proto-kit/protocol";
+import { VanillaProtocolModules, VanillaRuntimeModules,} from "@proto-kit/library";
+import { Sequencer, SequencerModulesRecord } from "@proto-kit/sequencer";
+import { container } from "tsyringe";
+import { PrivateKey } from "o1js";
+import { GraphqlClient } from "@proto-kit/sdk";
+import { GraphqlQueryTransportModule } from "@proto-kit/sdk";
+import { GraphqlNetworkStateTransportModule } from "@proto-kit/sdk";
+import { GraphqlTransactionSender } from "@proto-kit/sdk";
+import { InMemorySigner } from "@proto-kit/sdk";
+import { AppChain, AppChainModulesRecord } from "@proto-kit/sdk";
+
+export {
+  CustomAppChain,
+  type ComputedBlockJSON,
+  type ComputedTransactionJSON,
+  type ChainState
+}
+
+
+/**
+ * Chain state, block and transaction types
+ */
+interface ComputedTransactionJSON {
+  argsFields: string[];
+  argsJSON: string[];
+  methodId: string;
+  nonce: string;
+  sender: string;
+  signature: {
+    r: string;
+    s: string;
+  };
+}
+
+interface ComputedBlockJSON {
+  txs?: {
+    status: boolean;
+    statusMessage?: string;
+    tx: ComputedTransactionJSON;
+  }[];
+}
+
+interface ChainState {
+  loading: boolean;
+  block?: {
+    height: string;
+  } & ComputedBlockJSON;
+}
+
+
+/**
+ * The custom appChain configured to use InMemorySigner
+ * and able to acces a remote sequencer (by it's Graphql endpoint) 
+ */
+class CustomAppChain<
+  RuntimeModules extends RuntimeModulesRecord,
+  ProtocolModules extends ProtocolModulesRecord &
+    MandatoryProtocolModulesRecord,
+  SequencerModules extends SequencerModulesRecord,
+  AppChainModules extends AppChainModulesRecord,
+> extends AppChain<
+  RuntimeModules,
+  ProtocolModules,
+  SequencerModules,
+  AppChainModules
+> {
+  public static fromRuntime<RuntimeModules extends RuntimeModulesRecord>(
+    runtimeModules: RuntimeModules
+  ) {
+    const appChain = new CustomAppChain({
+      Runtime: Runtime.from({
+        modules: VanillaRuntimeModules.with(runtimeModules),
+      }),
+      Protocol: Protocol.from({
+        modules: VanillaProtocolModules.with({}),
+      }),
+      Sequencer: Sequencer.from({
+        modules: {},
+      }),
+
+      modules: {
+        GraphqlClient,
+        Signer: InMemorySigner,
+        TransactionSender: GraphqlTransactionSender,
+        QueryTransportModule: GraphqlQueryTransportModule,
+        NetworkStateTransportModule: GraphqlNetworkStateTransportModule,
+      },
+    });
+
+    appChain.configurePartial({
+      Sequencer: {},
+      Protocol: {
+        BlockProver: {},
+        StateTransitionProver: {},
+        AccountState: {},
+        BlockHeight: {},
+        LastStateRoot: {},
+        TransactionFee: {
+          tokenId: 0n,
+          feeRecipient: PrivateKey.random().toPublicKey().toBase58(),
+          baseFee: 0n,
+          perWeightUnitFee: 1n,
+          methods: {
+            "Faucet.drip": {
+              baseFee: 0n,
+              weight: 0n,
+              perWeightUnitFee: 0n,
+            },
+          },
+        },
+      },
+
+      Signer: { 
+        // we need to setup an initial random private key here that needs
+        // to be replaced later (when reconfiguring the appChain) by the 
+        // real signer private key
+        signer: PrivateKey.random() 
+      },
+      TransactionSender: {},
+      QueryTransportModule: {},
+      NetworkStateTransportModule: {},
+      GraphqlClient: {
+        // we need to setup an initial localhost url here that needs
+        // to be replaced later (when reconfiguring the appChain) by the 
+        // real url to the remote sequencer
+        url: "http://127.0.0.1:8080/graphql",
+      },
+    });
+
+    /**
+     * Register state service provider globally,
+     * to avoid providing an entire sequencer.
+     *
+     * Alternatively we could register the state service provider
+     * in runtime's container, but i think the event emitter proxy
+     * instantiates runtime/runtime modules before we can register
+     * the mock state service provider.
+     */
+    const stateServiceProvider = new StateServiceProvider();
+    stateServiceProvider.setCurrentStateService(new InMemoryStateService());
+    container.registerInstance("StateServiceProvider", stateServiceProvider);
+
+    return appChain;
+  }
+
+  public async start() {
+    log.setLevel("ERROR");
+    await super.start();
+  }
+
+  /**
+   * Wait for a block to be produced and includes transactions.
+   * The produced block: 
+   * @returns the produced block
+   */
+  public async waitForBlock(): Promise<ChainState> {
+    let state: ChainState = {
+      loading: true,
+      block: undefined
+    }
+    
+    while (!(state.block && (state.block as any).txs?.length)) {
+      console.log("Polling block state"); 
+      await delay(2); // 2 seconds
+
+      const gqlEndpoint = (this.config.GraphqlClient as any).url;
+      const response = await fetch(gqlEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(gqlGetBlockQuery),
+      });
+
+      const { data } = (await response.json()) as BlockQueryResponse;
+
+      state = chainState(data); 
+      // console.log("data: ", JSON.stringify(data, null, 2)); 
+      // console.log("state: ", JSON.stringify(state, null, 2)); 
+    }
+
+    return state;
+  }
+}
+
+
+/** 
+ * Block state and wait helpers
+ */
+
+
+const chainState = (data: any) => {
+  return {
+    loading: false,
+    block: data.network.unproven
+      ? {
+          height: data.network.unproven.block.height,
+          ...data.block,
+        }
+      : undefined
+  } 
+};
+
+const gqlGetBlockQuery = {
+  query: `
+    query GetBlock {
+      block {
+        txs {
+          tx {
+            argsFields
+            argsJSON
+            methodId
+            nonce
+            sender
+            signature {
+              r
+              s
+            }
+          }
+          status
+          statusMessage
+        }
+      }
+      network {
+        unproven {
+          block {
+            height
+          }
+        }
+      }
+    }
+  `,
+}
+
+interface BlockQueryResponse {
+  data: {
+    network: {
+      unproven?: {
+        block: {
+          height: string;
+        };
+      };
+    };
+    block: ComputedBlockJSON;
+  };
+}
+
+function delay(secs: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, secs*1000));
+}
---
+++ packages/chain/test/custom-client.test.ts
@@ -0,0 +1,62 @@
+import { PrivateKey } from "o1js";
+import { log } from "@proto-kit/common";
+import { BalancesKey, TokenId, UInt64 } from "@proto-kit/library";
+import { Balances } from "../src/balances";
+import { ChainState, CustomAppChain } from "../src/custom-app-chain";
+
+log.setLevel("ERROR");
+
+describe("Custom appChain", () => {
+  it("configure, run and send txn with a CustomAppChain", async () => {
+
+    // get some test signer info
+    const alicePrivateKey = PrivateKey.random();
+    const alice = alicePrivateKey.toPublicKey();
+    const tokenId = TokenId.from(0);
+
+    // configure the custom AppChain 
+    const appChain = CustomAppChain.fromRuntime({
+      Balances,
+    });
+
+    appChain.configurePartial({
+      Runtime: {
+        Balances: {
+          totalSupply: UInt64.from(10000),
+        },
+      },
+      // CAUTION: we need to setup the signer here NOT in txn.sign() !
+      // it would be good if this can be fixed 
+      Signer: { 
+        signer: alicePrivateKey 
+      },
+      // CAUTION: we need to setup the sequencer URL here if the sequencer 
+      // is running in some remote host and not in localhost
+      GraphqlClient: {
+        url: "http://127.0.0.1:8080/graphql",
+      },
+    });
+
+    await appChain.start();
+
+    // setup the runtimeModule we will use and send the transaction
+    const balances = appChain.runtime.resolve("Balances");
+
+    const tx1 = await appChain.transaction(alice, () => {
+      balances.addBalance(tokenId, alice, UInt64.from(1000));
+    });
+    await tx1.sign();
+    await tx1.send();
+
+    // wait for the produced block containing this transaction 
+    let chainState = await appChain.waitForBlock() as ChainState;
+    let transaction = (chainState?.block?.txs as any[])[0];
+    expect(transaction.status).toBe(true);
+    console.log("Transaction: ", JSON.stringify(transaction.tx, null, 2))
+
+    // access the runtimeModule props 
+    const key = new BalancesKey({ tokenId, address: alice });
+    const balance = await appChain.query.runtime.Balances.balances.get(key);
+    expect(balance?.value.toBigInt()).toBe(1000n);
+  }, 1_000_000);
+});
