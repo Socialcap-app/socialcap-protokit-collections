12d3f97175b9077d39fa8450218a0cbb3983fc4e
Mario Zito <mario.zito@treme.io>
2024-06-18 16:50:29 -0300

    Minor changes to listener

--- packages/chain/run-listener.sh
+++ packages/chain/run-listener.sh
@@ -1,6 +1,9 @@
+#/bin/bash
+pnpm build
+
 node \
   --loader ts-node/esm \
   --experimental-vm-modules \
   --experimental-wasm-modules \
   --experimental-wasm-threads \
-  ./src/nats-listener.ts
+  ./src/main-nats-listener.ts
